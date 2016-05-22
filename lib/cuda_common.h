bfeb4705e4e27214c87a12d34ccdc38ae97ad6f8
bulatz <bulat.ziganshin@gmail.com>
2016-05-23 01:14:16 +0300

    Lib: DisplayCudaDevice() added printing of VRAM speed & bitness

--- lib/cuda_common.h
+++ lib/cuda_common.h
@@ -1,4 +1,33 @@
 #include <helper_cuda.h>       // helper functions for CUDA error checking and initialization
+#include <cuda_runtime.h>
+
+
+
+// CUDA-C includes
+#include <cuda.h>
+
+// This function wraps the CUDA Driver API into a template function
+template <class T>
+inline void getCudaAttribute(T *attribute, CUdevice_attribute device_attribute, int device)
+{
+    CUresult error =    cuDeviceGetAttribute(attribute, device_attribute, device);
+
+    if (CUDA_SUCCESS != error)
+    {
+        fprintf(stderr, "cuSafeCallNoSync() Driver API error = %04d from file <%s>, line %i.\n",
+                error, __FILE__, __LINE__);
+
+        // cudaDeviceReset causes the driver to clean up all state. While
+        // not mandatory in normal operation, it is good practice.  It is also
+        // needed to ensure correct operation when the application is being
+        // profiled. Calling cudaDeviceReset causes all profile data to be
+        // flushed before the application exits
+        cudaDeviceReset();
+        exit(EXIT_FAILURE);
+    }
+}
+
+
 
 // Display CUDA device
 inline void DisplayCudaDevice()
@@ -11,12 +40,19 @@ inline void DisplayCudaDevice()
     checkCudaErrors(cudaGetDevice(&dev));
     checkCudaErrors(cudaGetDeviceProperties(&deviceProp, dev));
 
+    // This only available in CUDA 4.0-4.2 (but these were only exposed in the CUDA Driver API)
+    int memoryClock, memBusWidth;
+    getCudaAttribute<int>(&memoryClock, CU_DEVICE_ATTRIBUTE_MEMORY_CLOCK_RATE, dev);
+    getCudaAttribute<int>(&memBusWidth, CU_DEVICE_ATTRIBUTE_GLOBAL_MEMORY_BUS_WIDTH, dev);
+
     int sm_per_multiproc  =  deviceProp.major == 9999 && deviceProp.minor == 9999
                                 ? 1
                                 : _ConvertSMVer2Cores(deviceProp.major, deviceProp.minor);
 
     // printf("Device %d", dev);
-    printf("%s, SM %d.%d, %.1lf GB VRAM", deviceProp.name, deviceProp.major, deviceProp.minor, deviceProp.totalGlobalMem/double(1<<30));
+    printf("%s, SM %d.%d.  %.1lf GB VRAM", deviceProp.name, deviceProp.major, deviceProp.minor, deviceProp.totalGlobalMem/double(1<<30));
+    printf(" (%.0lf MHz * %d-bit = %.0lf GB/s)", memoryClock*0.001, memBusWidth, 1e-6*memoryClock*memBusWidth/4);
     printf(".  %d SM * %d alu * %.0lf MHz = %.0lf GIOPS\n", deviceProp.multiProcessorCount, sm_per_multiproc, deviceProp.clockRate*0.001,
                                                      1e-6 * deviceProp.multiProcessorCount * sm_per_multiproc * deviceProp.clockRate);
+
 }
--- mtf/compile-cuda.cmd
+++ mtf/compile-cuda.cmd
@@ -3,7 +3,7 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_
 mkdir m:\cuda-temp 2>nul
 set opts=-m64 -O3 --expt-extended-lambda -keep --keep-dir m:\cuda-temp
 ::  --resource-usage -lineinfo
-"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v7.5\bin\nvcc" %opts% -I../lib -I../bwt "-IC:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v7.5\include" "-IC:\Documents and Settings\All Users\NVIDIA Corporation\CUDA Samples\v7.5\common\inc" -IC:\Base\Compiler\GPGPU\cub-1.5.2 %*
+"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v7.5\bin\nvcc" %opts% -I../lib -I../bwt "-IC:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v7.5\include" "-IC:\Documents and Settings\All Users\NVIDIA Corporation\CUDA Samples\v7.5\common\inc" -IC:\Base\Compiler\GPGPU\cub-1.5.2 cuda.lib %*
 del *.exp *.lib 2>nul
 ::"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v7.5\bin\ptxas" -arch sm_52 mtf.ptx
 ::"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v7.5\bin\nvdisasm.exe" elf.o >mtf.sm_52.sass
