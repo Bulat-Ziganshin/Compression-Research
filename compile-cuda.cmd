d8e8f52bdf2de571bdf4bfd9e86707acfd66b567
bulatz <bulat.ziganshin@gmail.com>
2016-06-04 21:24:52 +0300

    BSL: added ability to compile without CUDA and gcc compilation script

--- compile-cuda.cmd
+++ compile-cuda.cmd
@@ -1,9 +1,11 @@
 @echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
 mkdir m:\cuda-temp 2>nul
+set defines=
 set opts=-m64 -O3 --expt-extended-lambda -lineinfo -keep --keep-dir m:\cuda-temp
-::  --resource-usage timer 
-"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v7.5\bin\nvcc" %opts% -IC:\Base\Compiler\GPGPU\cub-1.5.2\cub "-IC:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v7.5\include" "-IC:\Documents and Settings\All Users\NVIDIA Corporation\CUDA Samples\v7.5\common\inc" -IC:\Base\Compiler\GPGPU\cub-1.5.2 cuda.lib %*
+set includes=-IC:\Base\Compiler\GPGPU\cub-1.5.2 "-IC:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v7.5\include" "-IC:\Documents and Settings\All Users\NVIDIA Corporation\CUDA Samples\v7.5\common\inc"
+::  --resource-usage timer
+"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v7.5\bin\nvcc" %defines% %opts% %includes% bsl.cu cuda.lib -o bsl-cuda-x64.exe %*
 del *.exp *.lib 2>nul
 ::"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v7.5\bin\ptxas" -arch sm_52 mtf.ptx
 ::"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v7.5\bin\nvdisasm.exe" elf.o >mtf.sm_52.sass
---
+++ compile-gcc.cmd
@@ -0,0 +1,1 @@
+g++ -m64 -O3 -std=c++14 -s -static -Wno-write-strings bsl.cpp -o bsl-gcc-x64.exe %*
