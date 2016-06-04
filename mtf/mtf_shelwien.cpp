0d130d45d6ab15d8bf9d535a197e278b6b43696f
bulatz <bulat.ziganshin@gmail.com>
2016-06-04 22:39:15 +0300

    MTF: updated mtf_shelwien() - 114 MB/s with AVX2, 93 MB/s with SSE2

--- mtf/mtf_shelwien.cpp
+++ mtf/mtf_shelwien.cpp
@@ -1,21 +1,23 @@
 
-/* Vectorizable MTF algo by Eugene Shelwien (original: gmtf_v1.zip)
+/* Vectorizable MTF algo by Eugene Shelwien (original: gmtf_v2f.zip)
 */
 
 void mtf_shelwien (const byte* input, byte* output, int n)
 {
-    short Rank[ALPHABET_SIZE];
+    typedef signed char ranktype;
+    ranktype Rank[ALPHABET_SIZE];
 
     for (int i = 0; i < ALPHABET_SIZE; ++i)
-        Rank[i] = i;
+        Rank[i] = i-128;
 
     for (int i=0; i<n; i++)
     {
         auto c = *input++;
         auto d = Rank[c];
+#pragma unroll(16)
         for (int j=0; j<ALPHABET_SIZE; j++)
-            Rank[j] += ( Rank[j]<d );
-        Rank[c] = 0;
-        *output++ = d;
+            Rank[j] -= (Rank[j]<d)?ranktype(0xFF):ranktype(0);
+        Rank[c] = -128;
+        *output++ = d+128;
     }
 }
