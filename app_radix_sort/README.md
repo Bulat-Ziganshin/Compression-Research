5c97a94a2199d471911900f4c62ab1d9ece16d70
bulatz <bulat.ziganshin@gmail.com>
2016-06-06 05:05:49 +0300

    RadixSort: benchmark key+value sorting too!

--- app_radix_sort/README.md
+++ app_radix_sort/README.md
@@ -2,20 +2,49 @@ Benchmark CUB radix sort with various parameters.
 
 ### Current results with CUDA 7.5 and CUB 1.5.2
 ```
-GeForce GTX 560 Ti, CC 2.1.  1.0 GB VRAM (2004 MHz * 256-bit = 128 GB/s).  8 SM * 48 alu * 1800 MHz = 691 GIOPS
-Sorting 32M elements:
-1 of 4: Throughput = 3529.1539 MElements/s, Time = 0.00951 s
-2 of 4: Throughput = 1762.2191 MElements/s, Time = 0.01904 s
-3 of 4: Throughput = 1248.6999 MElements/s, Time = 0.02687 s
-4 of 4: Throughput =  922.8657 MElements/s, Time = 0.03636 s
+GeForce GTX 560 Ti, CC 2.1.  VRAM 1.0 GB, 2004 MHz * 256-bit = 128 GB/s.  8 SM * 48 alu * 1800 MHz * 2 = 1.38 TFLOPS
+Sorting 16M elements:
+1/4+0: Throughput = 3487.920 MElements/s, Time = 4.810 ms
+2/4+0: Throughput = 1745.233 MElements/s, Time = 9.613 ms
+3/4+0: Throughput = 1200.088 MElements/s, Time = 13.980 ms
+4/4+0: Throughput =  901.352 MElements/s, Time = 18.613 ms
 
-1 of 8: Throughput = 1983.0368 MElements/s, Time = 0.01692 s
-2 of 8: Throughput =  991.4804 MElements/s, Time = 0.03384 s
-3 of 8: Throughput =  728.3311 MElements/s, Time = 0.04607 s
-4 of 8: Throughput =  516.3670 MElements/s, Time = 0.06498 s
-5 of 8: Throughput =  437.8928 MElements/s, Time = 0.07663 s
-6 of 8: Throughput =  355.0213 MElements/s, Time = 0.09451 s
-7 of 8: Throughput =  298.3419 MElements/s, Time = 0.11247 s
-8 of 8: Throughput =  266.6209 MElements/s, Time = 0.12585 s
-Elapsed time = 18.042 seconds
+1/8+0: Throughput = 1974.093 MElements/s, Time = 8.499 ms
+2/8+0: Throughput =  987.452 MElements/s, Time = 16.990 ms
+3/8+0: Throughput =  713.954 MElements/s, Time = 23.499 ms
+4/8+0: Throughput =  512.924 MElements/s, Time = 32.709 ms
+5/8+0: Throughput =  432.296 MElements/s, Time = 38.810 ms
+6/8+0: Throughput =  353.732 MElements/s, Time = 47.429 ms
+7/8+0: Throughput =  298.917 MElements/s, Time = 56.127 ms
+8/8+0: Throughput =  266.235 MElements/s, Time = 63.017 ms
+
+1/4+4: Throughput = 2354.350 MElements/s, Time = 7.126 ms
+2/4+4: Throughput = 1174.615 MElements/s, Time = 14.283 ms
+3/4+4: Throughput =  864.405 MElements/s, Time = 19.409 ms
+4/4+4: Throughput =  603.807 MElements/s, Time = 27.786 ms
+
+1/4+8: Throughput = 1678.648 MElements/s, Time = 9.994 ms
+2/4+8: Throughput =  832.958 MElements/s, Time = 20.142 ms
+3/4+8: Throughput =  571.701 MElements/s, Time = 29.346 ms
+4/4+8: Throughput =  419.946 MElements/s, Time = 39.951 ms
+
+1/8+4: Throughput = 1446.440 MElements/s, Time = 11.599 ms
+2/8+4: Throughput =  723.684 MElements/s, Time = 23.183 ms
+3/8+4: Throughput =  501.736 MElements/s, Time = 33.438 ms
+4/8+4: Throughput =  368.421 MElements/s, Time = 45.538 ms
+5/8+4: Throughput =  305.185 MElements/s, Time = 54.974 ms
+6/8+4: Throughput =  251.235 MElements/s, Time = 66.779 ms
+7/8+4: Throughput =  214.403 MElements/s, Time = 78.251 ms
+8/8+4: Throughput =  189.606 MElements/s, Time = 88.485 ms
+
+1/8+8: Throughput = 1250.835 MElements/s, Time = 13.413 ms
+2/8+8: Throughput =  623.788 MElements/s, Time = 26.896 ms
+3/8+8: Throughput =  416.163 MElements/s, Time = 40.314 ms
+4/8+8: Throughput =  310.980 MElements/s, Time = 53.950 ms
+5/8+8: Throughput =  253.448 MElements/s, Time = 66.196 ms
+6/8+8: Throughput =  210.652 MElements/s, Time = 79.644 ms
+7/8+8: Throughput =  181.395 MElements/s, Time = 92.490 ms
+8/8+8: Throughput =  159.199 MElements/s, Time = 105.385 ms
+
+Elapsed time = 20.352 seconds
 ```
--- app_radix_sort/radix_sort.cu
+++ app_radix_sort/radix_sort.cu
@@ -22,7 +22,7 @@
 
 
 // Parameters
-const int defaultNumElements = 32*1024*1024;
+const int defaultNumElements = 16<<20;
 double MIN_BENCH_TIME = 0.5;  // mimimum seconds to run each bechmark
 
 
@@ -42,10 +42,8 @@ __global__ void fill_with_random (T *d_array, uint32_t size)
 }
 
 
-//template <int sort_bytes, typename Key, typename Value>
-
 template <typename Key>
-double generic_sort (int SORT_BYTES, size_t n, void *d_array0, cudaEvent_t &start, cudaEvent_t &stop)
+double key_sort (int SORT_BYTES, size_t n, void *d_array0, cudaEvent_t &start, cudaEvent_t &stop)
 {
     // Bit subrange [begin_bit, end_bit) of differentiating key bits
     int begin_bit = 0,  end_bit = SORT_BYTES*8;
@@ -92,8 +90,59 @@ double generic_sort (int SORT_BYTES, size_t n, void *d_array0, cudaEvent_t &star
 }
 
 
+template <typename Key, typename Value>
+double keyval_sort (int SORT_BYTES, size_t n, void *d_array0, cudaEvent_t &start, cudaEvent_t &stop)
+{
+    // Bit subrange [begin_bit, end_bit) of differentiating key bits
+    int begin_bit = 0,  end_bit = SORT_BYTES*8;
+
+    auto d_array = (Key*) d_array0;
+    auto d_value_array = (Value*) (d_array + 2*n);
+
+    // Create a DoubleBuffer to wrap the pair of device pointers
+    cub::DoubleBuffer<Key> d_keys (d_array, d_array + n);
+    cub::DoubleBuffer<Value> d_values (d_value_array, d_value_array + n);
+
+    // Determine temporary device storage requirements
+    void     *d_temp_storage = NULL;
+    size_t   temp_storage_bytes = 0;
+    checkCudaErrors( cub::DeviceRadixSort::SortPairs (d_temp_storage, temp_storage_bytes, d_keys, d_values, n, begin_bit, end_bit));
+
+    // Allocate temporary storage
+    checkCudaErrors( cudaMalloc (&d_temp_storage, temp_storage_bytes));
+
+    int numIterations = 0;
+    double totalTime = 0;
+
+    for ( ; totalTime < MIN_BENCH_TIME; numIterations++)
+    {
+        // Fill source buffer with random numbers
+        fill_with_random<Key> <<< n/1024+1, 1024 >>> (d_array, n);
+        checkCudaErrors( cudaDeviceSynchronize());
+
+        checkCudaErrors( cudaEventRecord (start, nullptr));
+
+        // Run sorting operation
+        checkCudaErrors( cub::DeviceRadixSort::SortPairs (d_temp_storage, temp_storage_bytes, d_keys, d_values, n, begin_bit, end_bit));
+
+        // Record time
+        checkCudaErrors( cudaEventRecord (stop, nullptr));
+        checkCudaErrors( cudaDeviceSynchronize());
+        float start_stop;
+        checkCudaErrors( cudaEventElapsedTime (&start_stop, start, stop));
+        totalTime += start_stop/1000; // converts milliseconds to seconds
+    }
+
+    // Release temporary storage
+    checkCudaErrors( cudaFree (d_temp_storage));
+
+    return totalTime/numIterations;
+}
+
+
 int main (int argc, char **argv)
 {
+    bool full = false;
     int numElements = defaultNumElements;
 
     if (argc > 1) {
@@ -109,19 +158,41 @@ int main (int argc, char **argv)
     DisplayCudaDevice();
 
     void* d_array;
-    checkCudaErrors( cudaMalloc(&d_array, 2*numElements*sizeof(uint64_t)));
+    checkCudaErrors( cudaMalloc(&d_array, 4*numElements*sizeof(uint64_t)));
 
     cudaEvent_t start, stop;
     checkCudaErrors( cudaEventCreate(&start));
     checkCudaErrors( cudaEventCreate(&stop));
 
-    auto print = [&] (int bytes, int keysize, double totalTime) {
-        printf("%d of %d: Throughput = %.4lf MElements/s, Time = %.5lf s\n",
-               bytes, keysize, 1e-6 * numElements / totalTime, totalTime);
+    auto print = [&] (int bytes, int keysize, int valsize, double totalTime) {
+        printf("%d/%d+%d: Throughput =%9.3lf MElements/s, Time = %.3lf ms\n",
+               bytes, keysize, valsize, 1e-6 * numElements / totalTime, totalTime*1000);
     };
 
     printf("Sorting %dM elements:\n", numElements>>20);
-    for(int i=1;i<=4;i++)  print (i, 4, generic_sort <uint32_t> (i, numElements, d_array, start, stop));  printf("\n");
-    for(int i=1;i<=8;i++)  print (i, 8, generic_sort <uint64_t> (i, numElements, d_array, start, stop));
+    if (full)   {for(int i=1;i<=1;i++)  print (i, 1, 0, key_sort <uint8_t>  (i, numElements, d_array, start, stop));  printf("\n");}
+    if (full)   {for(int i=1;i<=2;i++)  print (i, 2, 0, key_sort <uint16_t> (i, numElements, d_array, start, stop));  printf("\n");}
+                {for(int i=1;i<=4;i++)  print (i, 4, 0, key_sort <uint32_t> (i, numElements, d_array, start, stop));  printf("\n");}
+                {for(int i=1;i<=8;i++)  print (i, 8, 0, key_sort <uint64_t> (i, numElements, d_array, start, stop));  printf("\n");}
+
+    if (full)   {for(int i=1;i<=1;i++)  print (i, 1, 1, keyval_sort <uint8_t,uint8_t>  (i, numElements, d_array, start, stop));  printf("\n");}
+    if (full)   {for(int i=1;i<=1;i++)  print (i, 1, 2, keyval_sort <uint8_t,uint16_t> (i, numElements, d_array, start, stop));  printf("\n");}
+    if (full)   {for(int i=1;i<=1;i++)  print (i, 1, 4, keyval_sort <uint8_t,uint32_t> (i, numElements, d_array, start, stop));  printf("\n");}
+    if (full)   {for(int i=1;i<=1;i++)  print (i, 1, 8, keyval_sort <uint8_t,uint64_t> (i, numElements, d_array, start, stop));  printf("\n");}
+
+    if (full)   {for(int i=1;i<=2;i++)  print (i, 2, 1, keyval_sort <uint16_t,uint8_t>  (i, numElements, d_array, start, stop));  printf("\n");}
+    if (full)   {for(int i=1;i<=2;i++)  print (i, 2, 2, keyval_sort <uint16_t,uint16_t> (i, numElements, d_array, start, stop));  printf("\n");}
+    if (full)   {for(int i=1;i<=2;i++)  print (i, 2, 4, keyval_sort <uint16_t,uint32_t> (i, numElements, d_array, start, stop));  printf("\n");}
+    if (full)   {for(int i=1;i<=2;i++)  print (i, 2, 8, keyval_sort <uint16_t,uint64_t> (i, numElements, d_array, start, stop));  printf("\n");}
+
+    if (full)   {for(int i=1;i<=4;i++)  print (i, 4, 1, keyval_sort <uint32_t,uint8_t>  (i, numElements, d_array, start, stop));  printf("\n");}
+    if (full)   {for(int i=1;i<=4;i++)  print (i, 4, 2, keyval_sort <uint32_t,uint16_t> (i, numElements, d_array, start, stop));  printf("\n");}
+                {for(int i=1;i<=4;i++)  print (i, 4, 4, keyval_sort <uint32_t,uint32_t> (i, numElements, d_array, start, stop));  printf("\n");}
+                {for(int i=1;i<=4;i++)  print (i, 4, 8, keyval_sort <uint32_t,uint64_t> (i, numElements, d_array, start, stop));  printf("\n");}
+
+    if (full)   {for(int i=1;i<=8;i++)  print (i, 8, 1, keyval_sort <uint64_t,uint8_t>  (i, numElements, d_array, start, stop));  printf("\n");}
+    if (full)   {for(int i=1;i<=8;i++)  print (i, 8, 2, keyval_sort <uint64_t,uint16_t> (i, numElements, d_array, start, stop));  printf("\n");}
+                {for(int i=1;i<=8;i++)  print (i, 8, 4, keyval_sort <uint64_t,uint32_t> (i, numElements, d_array, start, stop));  printf("\n");}
+                {for(int i=1;i<=8;i++)  print (i, 8, 8, keyval_sort <uint64_t,uint64_t> (i, numElements, d_array, start, stop));  printf("\n");}
     return 0;
 }
